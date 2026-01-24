04fce948b8363b23370f43021cdf437cbdd9beb9
StephenB <stephenbrad515@gmail.com>
2026-01-24 11:25:18 -0500

    Initial commit

---
+++ Testing1/u1_main.tf
@@ -0,0 +1,44 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "ubuntu-vpc"
+  auto_create_subnetworks = true
+}
+
+resource "google_compute_firewall" "ssh_rule" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "ubuntu_vm" {
+  name         = "ubuntu-server"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.ubuntu_image
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {}
+  }
+/*
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.public_key_path)}"
+  }
+  */
+}
