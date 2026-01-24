2e224e95292696fababf902c33327c02138da227
StephenB <stephenbrad515@gmail.com>
2026-01-24 15:02:03 -0500

    Create systemd service to mount bucket

--- Testing1/install_gcsfuse.sh
+++ Testing1/install_gcsfuse.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+gcloud secrets versions access 1 --secret=first
+
+
 # Update and install dependencies
 sudo apt-get update
 sudo apt-get install -y curl lsb-release
@@ -26,3 +29,35 @@ sed -i '/#user_allow_other/s/^[[:blank:]]*#//' /etc/fuse.conf
 # 5. Optional: Mount the bucket (Replace 'your-bucket-name' or use a variable)
 # Note: Mounting at boot usually requires a specific service account.
 gcsfuse thisisalongcrazyname9 /mnt/gcs-bucket
+
+#Create A systemd service to mount the bucket with gcsfuse
+echo "[Unit]
+Description=Mount GCS bucket my-bucket
+After=network-online.target local-fs.target
+Wants=network-online.target
+Requires=network-online.target
+
+[Service]
+Type=simple
+User=root
+Environment=GOOGLE_APPLICATION_CREDENTIALS=/data/gcp-key.json
+ExecStartPre=/bin/mkdir -p /mnt/gcs-bucket
+ExecStart=/usr/bin/gcsfuse --foreground --file-mode=777 --dir-mode=777 thisisalongcrazyname9 /mnt/gcs-bucket
+ExecStop=/bin/umount -l /mnt/gcs-bucket
+Restart=on-failure
+RestartSec=3
+TimeoutStartSec=30
+
+[Install]
+WantedBy=multi-user.target
+
+#/etc/systemd/system/gcsfuse-my-bucket.service" | tee /etc/systemd/system/gcsfuse-my-bucket.service
+
+# Reload configurations
+sudo systemctl daemon-reload
+
+# Enable automatic startup
+sudo systemctl enable gcsfuse-my-bucket.service
+
+# Start the service
+sudo systemctl start gcsfuse-my-bucket.service
