2ed5c51d559466ad4a4f5a756cbb6c4e06da6edf
StephenB <stephenbrad515@gmail.com>
2026-01-24 12:27:09 -0500

    added bucket creation files fix3

---
+++ Testing1/b1_main.tf
@@ -0,0 +1,30 @@
+resource "google_storage_bucket" "static_site" {
+  name          = var.bucket_name
+  location      = var.region
+  storage_class = var.storage_class
+  project       = var.project_id
+
+  # Force destroy allows terraform to delete the bucket even if it has files in it
+  force_destroy = true
+
+  # Prevents accidental deletion of older versions of files
+  versioning {
+    enabled = true
+  }
+
+  # Automatically move files to Nearline storage after 30 days to save money
+  lifecycle_rule {
+    condition {
+      age = 30
+    }
+    action {
+      type = "SetStorageClass"
+      storage_class = "NEARLINE"
+    }
+  }
+}
+
+output "bucket_url" {
+  value       = google_storage_bucket.static_site.url
+  description = "The URI of the created bucket"
+}
