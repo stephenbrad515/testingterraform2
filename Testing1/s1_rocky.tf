5d8d2fbdf81d0312f77d91c9cb8bcfcdd9e1adea
StephenB <stephenbrad515@gmail.com>
2026-02-01 20:40:30 -0500

    rocky

---
+++ Testing1/s1_rocky.tf
@@ -0,0 +1,62 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "instance-20260202-011910" {
+  boot_disk {
+    auto_delete = true
+    device_name = "rocky01"
+
+    initialize_params {
+      image = "projects/rocky-linux-cloud/global/images/rocky-linux-10-optimized-gcp-v20260115"
+      size  = 20
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-medium"
+  name         = "instance-20260202-011910"
+
+    metadata = {
+    startup-script = "metadata_startup_script = file("${path.module}/install_gcsfuse.sh")"
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/testingterraform2/regions/us-east4/subnetworks/ubuntu-vpc"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "42488166641-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "us-east4-b"
+}
